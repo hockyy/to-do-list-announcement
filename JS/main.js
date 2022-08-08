72fe0369ba254757cacd370e4bd0c1642ba53456
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-08-08 10:21:37 +0700

    Add time

--- JS/main.js
+++ JS/main.js
@@ -223,34 +223,34 @@ function changeTheme(color) {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+let countDownDate = new Date("Aug 9, 2020 15:37:25").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+let countDownFunc = function() {
 
   // Get today's date and time
-  var now = new Date().getTime();
+  let now = new Date().getTime();
 
   // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  let distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   let secString = String(seconds).padStart(2, '0');
   let minString = String(minutes).padStart(2, '0');
-  // Display the result in the element with id="demo"
-  if(!pauseState) {
+  if(pauseState) {
       document.getElementById("demo").innerHTML = hours + ":"
-      + minutes + ":" + secString ;
+      + minString + ":" + secString ;
   }
 
   // If the count down is finished, write some text
   if (distance < 0) {
-    clearInterval(x);
     document.getElementById("demo").innerHTML = "COUNTDOWN ENDS";
   }
-}, 1000);
+}
+
+// Update the count down every 1 second
+let x = setInterval(countDownFunc, 1000);
